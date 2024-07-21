273fd3b1af06d69ee9698be94aa483aabf50def4
郑皓文 <zhenghw21@mails.tsinghua.edu.cn>
2024-07-22 02:13:20 +0800

    feat: save keyframes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
 from utils.map import Map
 from utils.visualize_efficient import make_video
+import pickle
 
 mp = Map('./config/config.json')
 
@@ -8,5 +10,13 @@ mp.simulate()
 print('Simulation complete.')
 
 keyframes = mp.keyframes
+
+keyframes_file = './keyframes.pkl'
+with open(keyframes_file, 'wb') as f:
+    pickle.dump(keyframes, f)
+
+print('Keyframes saved.')
+
+print('Generating video...')
 
 make_video(keyframes, mp.width, mp.height, mp.walls, mp.ptcl_radius, output_filename='output.mp4')
