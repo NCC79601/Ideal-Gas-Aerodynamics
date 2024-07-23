1072ee92a0123b02d3f2706dbc4b3682198872f5
郑皓文 <zhenghw21@mails.tsinghua.edu.cn>
2024-07-24 02:54:35 +0800

    fix: use correct Maxwell-Boltzmann speed distribution

--- test_ptcl_sampler.py
+++ test_ptcl_sampler.py
@@ -1,7 +1,7 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-from utils.ptcl_sampler import ptcl_sampler
+from utils.ptcl_sampler import ptcl_sampler, maxwell_boltzmann_dist
 
 with open('./config/config.json', 'r') as f:
     config = json.load(f)
@@ -10,7 +10,7 @@ T0 = config['T0']
 ptcl_relative_mass = config['ptcl_relative_mass']
 
 ptcl_pos, ptcl_v = ptcl_sampler(
-    ptcl_count    = 1000,
+    ptcl_count    = 10000,
     map_size      = [100, 100],
     temperature   = T0,
     relative_mass = ptcl_relative_mass,
@@ -21,8 +21,8 @@ ptcl_pos, ptcl_v = ptcl_sampler(
 # Calculate the magnitude of ptcl_v
 ptcl_v_mag = np.linalg.norm(ptcl_v, axis=1)
 
-sample_mean = np.mean(ptcl_v_mag)
-sample_rms = np.sqrt(np.mean(ptcl_v_mag**2))
+sample_mean    = np.mean(ptcl_v_mag)
+sample_rms     = np.sqrt(np.mean(ptcl_v_mag**2))
 speed_of_sound = sample_rms * np.sqrt(1.4 / 3)
 
 print(f'Sample mean velocity: {sample_mean}')
@@ -30,8 +30,15 @@ print(f'Sample rms  velocity: {sample_rms}')
 print(f'Approximated speed of sound: {speed_of_sound}')
 
 # Plot the histogram
-plt.hist(ptcl_v_mag, bins=20)
-plt.xlabel('Magnitude of ptcl_v')
+plt.hist(ptcl_v_mag, bins=50)
+
+# Plot the Maxwell-Boltzmann Distribution
+v = np.linspace(0, 2000, 1000)
+plt.plot(v, maxwell_boltzmann_dist(v, T0, ptcl_relative_mass / 1000) * 10000)
+plt.legend(['Maxwell-Boltzmann Distribution', 'Histogram'])
+plt.grid(True)
+
+plt.xlabel('v')
 plt.ylabel('Frequency')
-plt.title('Histogram of ptcl_v Magnitude')
+plt.title('Histogram of Speed Distribution')
 plt.show()
--- utils/ptcl_sampler.py
+++ utils/ptcl_sampler.py
@@ -9,8 +9,72 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 with open(os.path.join(os.path.dirname(__file__), 'constants.json'), 'r') as f:
     constants = json.load(f)
 
-k = constants['k'] # Boltzmann constant
+k          = constants['k'] # Boltzmann constant
+Ru         = constants['Ru'] # universal gas constant
 m_1_12_C12 = constants['m_1_12_C12'] # mass of 1/12 of C12
+
+
+def maxwell_boltzmann_dist(v: float, temperature: float, molar_mass: float) -> float:
+    '''
+    Maxwell-Boltzmann distribution function
+
+    Parameters:
+    - v: velocity (m/s)
+    - temperature: temperature of the system (K)
+    - molar_mass: molar mass of the particle (g)
+
+    Returns:
+    - float, the value of Maxwell-Boltzmann distribution function
+    '''
+    return 4.0 * np.pi * ((molar_mass / (2 * np.pi * Ru * temperature)) ** 1.5) * (v ** 2) * np.exp(-(molar_mass * (v ** 2)) / (2 * Ru * temperature))
+
+
+def maxwell_boltzmann_dist_torch(v: torch.Tensor, temperature: float, molar_mass: float) -> torch.Tensor:
+    '''
+    Maxwell-Boltzmann distribution function for PyTorch tensors
+
+    Parameters:
+    - v: velocity (m/s)
+    - temperature: temperature of the system (K)
+    - molar_mass: molar mass of the particle (g)
+
+    Returns:
+    - torch.Tensor, the value of Maxwell-Boltzmann distribution function
+    '''
+    return 4.0 * torch.pi * ((molar_mass / (2 * torch.pi * Ru * temperature)) ** 1.5) * (v ** 2) * torch.exp(-(molar_mass * (v ** 2)) / (2 * Ru * temperature))
+
+
+def sample_maxwell_boltzmann_dist(temperature: float, molar_mass: float, sample_count: int, sample_batch_size = 10000) -> np.ndarray:
+    '''
+    Sample from Maxwell-Boltzmann distribution for PyTorch tensors
+
+    Parameters:
+    - temperature: temperature of the system (K)
+    - molar_mass: molar mass of the particle (g)
+    - count: number of samples to generate
+
+    Returns:
+    - torch.Tensor, samples from Maxwell-Boltzmann distribution
+    '''
+    # Define the maximum value of the PDF for the given parameters
+    v_max = torch.sqrt(torch.Tensor([2 * Ru * temperature / molar_mass])).to(device).detach()
+    f_max = maxwell_boltzmann_dist_torch(torch.tensor(v_max, device=device), temperature, molar_mass)
+    
+    samples = []
+    
+    while len(samples) < sample_count:
+        v = torch.rand(sample_batch_size, device=device) * 5 * v_max  # Generate candidate velocities
+        f_v = maxwell_boltzmann_dist_torch(v, temperature, molar_mass)  # Calculate the PDF value at this velocity
+        u = torch.rand(sample_batch_size, device=device) * f_max  # Generate a uniform random number
+        mask = u < f_v
+        samples.extend(v[mask].tolist())
+
+    # trim if too many samples
+    if len(samples) > sample_count:
+        samples = samples[:sample_count]
+    
+    return np.array(samples)
+
 
 def ptcl_sampler(
     ptcl_count: int,
@@ -40,17 +104,15 @@ def ptcl_sampler(
     ptcl_pos = np.array([sampled_x, sampled_y]).T
 
     # sample velocity from Maxwell-Boltzmann distribution
-    mass = relative_mass * m_1_12_C12
-    scale = np.sqrt(k * temperature / mass)
-    sampled_v = np.random.rayleigh(scale, ptcl_count)
+    sampled_v = sample_maxwell_boltzmann_dist(temperature, relative_mass / 1000, ptcl_count)
     sampled_v[sampled_v > max_velocity] = max_velocity
     sampled_angle = np.random.uniform(0, 2 * np.pi, ptcl_count)
     sampled_v_x = sampled_v * np.cos(sampled_angle) + flow_velocity
     sampled_v_y = sampled_v * np.sin(sampled_angle)
     ptcl_v = np.array([sampled_v_x, sampled_v_y]).T
 
-    v_thermal_mean = np.sqrt(2 * np.pi * k * temperature / mass)
-    print(f'mean thermal velocity: {v_thermal_mean}')
+    v_thermal_rms = np.sqrt(3 * Ru * temperature / (relative_mass / 1000))
+    print(f'rms thermal velocity: {v_thermal_rms}')
 
     return ptcl_pos, ptcl_v
 
